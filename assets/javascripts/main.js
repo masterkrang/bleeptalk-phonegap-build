91a8799cc37a2c6d7f2b3d0a280be131ce50a8b1
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-05-29 14:57:23 -0700

    added support for custom id's in jquery mobile foursquare suggest ul's

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,6 @@
 $( '#main' ).live( 'pageinit', function(event) {
   //alert( 'This page was just enhanced by jQuery Mobile!' );
+	
 	$("#search").mobile_fs_suggest({
 		'client_id'		: '2RIEFGJ11QYCMK1KW2RECDINMBWEL2IMUWYUC4MGGYGQQBDL',
 		'client_secret'	: '02SA1F4ZLE3SRFJUM0RKLGJRVWS43M1EAG4DYZEFP1SFISU3'
@@ -7,5 +8,5 @@ $( '#main' ).live( 'pageinit', function(event) {
 	
 	// Options: retrieve the location every 3 seconds
 	//
-	var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { frequency: 3000 });
+	var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { frequency: 6000 });
 });
