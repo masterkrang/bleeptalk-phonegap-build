fe94ff85115bc32c6c84f084aa0225d352f418ce
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-05-29 01:05:36 -0700

    added custom jquery mobile foursquare suggest, will update later, added geolocation, some sass

---
+++ assets/javascripts/geolocation.js
@@ -0,0 +1,18 @@
+// onSuccess Callback
+//   This method accepts a `Position` object, which contains
+//   the current GPS coordinates
+//
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />'      + element.innerHTML;
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
